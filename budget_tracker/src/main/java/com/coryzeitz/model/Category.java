78b634a6283c5738e0ffdb41ddb6345fbd3d5f4c
cory <csz8115@rit.edu>
2025-10-27 19:04:34 -0400

    created project javafx project directory and defined the models

---
+++ budget_tracker/src/main/java/com/coryzeitz/model/Category.java
@@ -0,0 +1,14 @@
+package com.coryzeitz.model;
+
+public enum Category {
+    GROCERIES,
+    TRANSPORTATION,
+    UTILITIES,
+    ENTERTAINMENT,
+    HOUSING,
+    HEALTHCARE,
+    EDUCATION,
+    DEBT,
+    SAVINGS,
+    OTHER
+}
---
+++ budget_tracker/src/main/java/com/coryzeitz/model/Expense.java
@@ -0,0 +1,41 @@
+package com.coryzeitz.model;
+
+public class Expense {
+    private String title;
+    private String description;
+    private double value;
+    private Category category;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+}
